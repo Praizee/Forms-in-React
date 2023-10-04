2f45583fbc94e681e9a6aa8531eabd40a7826012
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-04 11:51:50 +0100

    Update Form01.jsx

--- src/Pages/Form01.jsx
+++ src/Pages/Form01.jsx
@@ -1,71 +1,62 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from "react";
 
 export const Form01 = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+    const [formData, setFormData] = useState({ name: "", email: "", message: "" });
+    const [inputValue, setInputValue] = useState('');
+    const [inputError, setInputError] = useState(null);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
+        );
+        if (inputValue.length >= 5) {
+            // submit form
+        } else {
+            setInputError('Input must be at least 5 characters');
+        }
+    };
+
+    // 
+
+    function handleInputChange(event) {
+        const value = event.target.value;
+        setInputValue(value);
+
+        if (value.length < 5) {
+            setInputError('Input must be at least 5 characters');
+        } else {
+            setInputError(null);
+        }
+    }
 
     return (
-        <section>
+        <form className=" p-8 block space-x-8"
+            onSubmit={handleSubmit}>
+            <label>
+                Fruit:
+                <input type="text" value={inputValue} onChange={handleInputChange} />
+            </label>
+            {inputError && <div className=" text-red-600 italic">{inputError}</div>}
+            <button type="submit">Submit</button>
 
-            <div>
+            {/* <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+            {inputError && <div className=" text-red-600 italic">{inputError}</div>}
 
-                {/* <span className="relative inline-block px-10">
-                    <span className="absolute left-0 top-1/2 transform -translate-y-1/2 h-0.5 bg-black w-1/4"></span>
-                    <span className="mx-2 text-gray-600">OR</span>
-                    <span className="absolute right-0 top-1/2 transform -translate-y-1/2 h-0.5 bg-black w-1/4"></span>
-                </span> */}
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+            {inputError && <div className=" text-red-600 italic">{inputError}</div>}
 
-                <form className=''
-                    onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        type="text"
-                        placeholder="First name"
-                        {...register("First name", { required: true, maxLength: 80 })}
-                        className='p-4 input rounded-md'
-                    />
-                    <input
-                        type="text"
-                        placeholder="Last name"
-                        {...register("Last name", { required: true, maxLength: 100 })}
-                        className='p-4 input rounded-md'
-                    />
-                    <input
-                        type="text" placeholder="Email"
-                        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
-                        className='p-4 input rounded-md'
-                    />
-                    <input
-                        type="tel"
-                        placeholder="Mobile number"
-                        {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })}
-                        className='p-4 input rounded-md'
-                    />
-                    <select {...register("Title", { required: true })}
-                    >
-                        <option value="Mr">Mr</option>
-                        <option value="Mrs">Mrs</option>
-                        <option value="Miss">Miss</option>
-                        <option value="Dr">Dr</option>
-                    </select>
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
+            {inputError && <div className=" text-red-600 italic">{inputError}</div>}
 
-                    <input
-                        {...register("Developer", { required: true })}
-                        type="radio"
-                        value="Yes"
-                        className='radio'
-                    />
-                    <input
-                        {...register("Developer", { required: true })}
-                        type="radio"
-                        value="No"
-                        className='radio'
-                    />
-
-                    <input type="submit" className='btn' />
-                </form>
-            </div>
-        </section>
+            <button type="submit">Submit</button> */}
+        </form>
     );
 }
